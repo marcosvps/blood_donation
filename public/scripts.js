b863959a867745d98b16d839fb0146dcb6ec4453
Marcos Porto <marcosporto.sa@gmail.com>
2020-05-08 22:09:44 -0300

    Initial Commit

---
+++ public/scripts.js
@@ -0,0 +1,8 @@
+document
+    .querySelector('header button')
+    .addEventListener("click", function () {
+        document
+            .querySelector('.form')
+            //.getElementsByClassName.display = "none"
+            .classList.toggle('hide')
+    })
